3dd927be81e7c54d02a63fa35b367ed3322fed29
jaywcjlove <398188662@qq.com>
2022-01-02 00:43:53 +0800

    website: update style.

--- website/index.tsx
+++ website/index.tsx
@@ -1,6 +1,5 @@
-import React from 'react';
 import ReactDOM from 'react-dom';
-import '@uiw/reset.css';
+import '@uiw/reset.css/reset.css';
 import App from './App';
 
 ReactDOM.render(<App />, document.getElementById('root'));
